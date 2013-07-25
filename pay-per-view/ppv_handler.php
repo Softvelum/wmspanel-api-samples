d587a2be7f6230ea16ef540a00d46a3c6127ec1f
WMSPanel <sales@wmspanel.com>
2013-07-25 12:53:53 +1100

    Update for handler

--- pay-per-view/ppv_handler.php
+++ pay-per-view/ppv_handler.php
@@ -4,6 +4,9 @@ $fp = fopen('/var/tmp/request.log', 'w');
 fwrite($fp, $HTTP_RAW_POST_DATA);
 fclose($fp);
 
+// Use this object for accessing each viewer's ID, IP and stream name.
+$sync_data = json_decode($HTTP_RAW_POST_DATA);
+
 // Return IDs of clients which needs to be denied. Their IDs are 1 and 2 here.
 // Those viewers will be disconnected immediatelly and will not be allowed to connect anymore.
 header('Content-Type: application/json');
