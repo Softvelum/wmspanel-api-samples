aa2e0b47a9eb0ebbd4e5c636159a7ab941a52a14
WMSPanel team <sales@wmspanel.com>
2014-08-18 18:50:19 +1100

    PPV sample

--- pay-per-view/ppv_media_signature.php
+++ pay-per-view/ppv_media_signature.php
@@ -2,7 +2,8 @@
 $today = gmdate("n/j/Y g:i:s A");
 
 // URL of media we want to handle with pay-per-view
-$initial_url = "rtsp://127.0.0.1:1935/vod/sample.mp4";
+$initial_url = "rtsp://127.0.0.1:1935/vod";
+$video_url = "/sample.mp4";
 // client ID which is defined based on customer's database of users
 $id = "5"; 
 // A password entered in WMSAuth rule via web interface
@@ -17,7 +18,7 @@ $urlsignature =
   'server_time=' . $today . '&hash_value=' . $base64hash . 
   '&validminutes=' . $validminutes . '&id=' . $id;
 $base64urlsignature = base64_encode($urlsignature);
-$signedurlwithvalidinterval = $initial_url . "?wmsAuthSign=$base64urlsignature";
+$signedurlwithvalidinterval = $initial_url . "?wmsAuthSign=$base64urlsignature" . $video_url;
 
 // New protected media URL
 echo $signedurlwithvalidinterval;
